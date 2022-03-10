d3f8df70e230d9d7ab109b838110b89d762e2f96
Hector Zaragoza <zaragoza09@gmail.com>
2022-03-10 15:25:16 -0800

    initial commit Binary Search Trees

---
+++ data_structures/Trees/binarySearchTrees.js
@@ -0,0 +1,5 @@
+// Trees
+// A data structure that consists of nodes in a parent/child relationship
+// nodes are connected via "branches"
+// Each node can point to more than 1 node.
+// Top most node is the "root" node.
