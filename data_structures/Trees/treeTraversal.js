8d2c82d152a3502260ae8ffae49e9b544a9418b0
Hector Zaragoza <zaragoza09@gmail.com>
2022-03-11 10:56:31 -0800

    inital tree traversal

---
+++ data_structures/Trees/treeTraversal.js
@@ -0,0 +1,11 @@
+// Tree traversal
+// Given any tree, How do we visit any node, once?
+// Remember, trees can be unsorted.
+// What would our path be to visit each node once? What's the most efficient path?
+
+// Two Main Ways to Traverse a tree
+//  1) Breadth-first Search (Horizontal approach, Breadth is across childrens, at each lower level, look to all children at that level)
+//  2) Depth-first Search (Vertical approach)
+//      a. in order
+//      b. pre order
+//      c. Post Order
