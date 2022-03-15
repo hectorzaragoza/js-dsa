021fde0267a0eb9f31bc4878c05e4a1619e9630f
Hector Zaragoza <zaragoza09@gmail.com>
2022-03-15 08:30:09 -0700

    BFS and DFS on binary search trees

--- data_structures/Trees/treeTraversal.js
+++ data_structures/Trees/treeTraversal.js
@@ -9,3 +9,19 @@
 //      a. in order
 //      b. pre order
 //      c. Post Order
+
+// Breadth First Search Intro (BFS)
+// Visit every node (each sibling) on a level, before moving down a level. I am working horizontally.
+// To do a BFS, we are going to do this iteratively by modelling a queue using a list or an array
+
+// Pesudocode
+/**
+ * Create a queue (this can be an array) and a variable to store the values of nodes visited
+ * Place the root node in the queue
+ * Loop as long as there is anything in the queue
+ *      Dequeue a node from the queue and push the value of the node into the variable that stores the nodes
+ *      If there is a left property on the node dequeued - add it to the queue
+ *      If there is a right property on the node dequeued - add it to the queue
+ * Return the variable that stores the values
+ */
+
